5061b484cf31a4bcee2f302f04d89c890e377abd
sambokar <sumit_ambokar@intuit.com>
2018-03-13 13:12:32 -0700

    Randomized urls

--- src/test/scala/com/intuit/sambokar/scenarios/GetImage.scala
+++ src/test/scala/com/intuit/sambokar/scenarios/GetImage.scala
@@ -1,16 +1,19 @@
 package com.intuit.sambokar.scenarios
-
+import com.intuit.sambokar.util._
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
-
 object GetImage {
-
+  val jsonUrlFeeder = jsonUrl(Environment.baseURL.concat("/getAllFileNames")).random
   val getImageHttp = http("get image")
-    .get("/getFile?fileName=1520944101975-homer.jpg")
+    .get("/getFile?fileName=${id}")
+    //.get("/getFile?fileName=1520944101975-homer.jpg")
     .header(HttpHeaderNames.Accept, HttpHeaderValues.ImageJpeg)
     .check(status.is(200))
 
   val getImage = scenario("get image")
+     .exec(http("get all image names")
+       .get("/getAllFileNames"))
+      .feed(jsonUrlFeeder)
      .exec(
        getImageHttp
      )
