f64bfa9a51b5cceb8c0a45e3c53a118f334c0ac5
sambokar <sumit_ambokar@intuit.com>
2018-03-18 22:18:07 -0700

    Simulating baseline like func with 500 concurrent users

--- src/test/scala/com/intuit/sambokar/simulation/ImageServiceBottleneckSimulation.scala
+++ src/test/scala/com/intuit/sambokar/simulation/ImageServiceBottleneckSimulation.scala
@@ -10,22 +10,34 @@ import scala.concurrent.duration._
 class ImageServiceBottleneckSimulation extends Simulation {
 
   val httpConf = http.baseURL(Environment.baseURL)
-                      //.headers(Headers.commonHeader)
+  //.headers(Headers.commonHeader)
 
   val imageServiceScenarios = List(
 
-    GetImage.getLargeImage.inject(
-      atOnceUsers(Environment.usersStressTest.toInt)
-    )
+    GetImage.getImage.inject(
+      atOnceUsers(Environment.fiveHundredUsers)
+      //rampUsers(100) over(1 seconds),
+      //constantUsersPerSec(1000) during(15 seconds)
+      //rampUsersPerSec(1) to 10 during(30 seconds) // 6
+      //rampUsersPerSec(10) to 20 during(10 minutes) randomized, // 7
+      //splitUsers(1000) into(rampUsers(10) over(10 seconds)) separatedBy(10 seconds), // 8
+      //splitUsers(1000) into(rampUsers(10) over(10 seconds)) separatedBy atOnceUsers(30), // 9
+      //heavisideUsers(1000) over(20 seconds) // 10
+    ),
+
+    PostImage.postImageNoDelete.inject(
+      atOnceUsers(Environment.fiveHundredUsers))
+    //.throttle(reachRps(1) in (20 seconds), holdFor(30 seconds))
+
   )
 
   setUp(imageServiceScenarios)
     .protocols(httpConf)
-    .maxDuration(Environment.maxTestDurationMins.toInt minutes)
+    .maxDuration(Environment.testDurationFiveMins minutes)
     .assertions(
-      global.responseTime.max.lessThan(Environment.maxResponseTime.toInt),
-      global.successfulRequests.percent.greaterThan(Environment.reqPassRate95Min.toInt),
-      global.responseTime.percentile3.lessThan(Environment.maxRespTimeFor95Pass.toInt)
-      //global.failedRequests.count.is(0)
+      //global.responseTime.max.lessThan(Environment.maxResponseTime.toInt),
+      //global.successfulRequests.percent.greaterThan(Environment.reqPassRate95Min.toInt),
+      //global.responseTime.percentile3.lessThan(Environment.maxRespTimeFor95Pass.toInt)
+      global.failedRequests.percent.lessThan(10)
     )
 }
